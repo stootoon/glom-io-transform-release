94e333e55789d274005e7870ae5521200e989896
Sina Tootoonian <sina.tootoonian@gmail.com>
2026-08-18 08:46:08 +0100

    Update paths.

--- glom_io_transform/paths.py
+++ glom_io_transform/paths.py
@@ -1,8 +1,11 @@
 import os, sys
 from pathlib import Path
-proj_path = os.path.join(os.environ["GLOM_IO"], "glom_io_transform") 
+proj_path = os.environ["GLOM_IO"] 
 data_root = os.environ["GLOM_IO_DATA"]
 fits_root = os.path.join(proj_path, "model_fitting")
+assert os.path.exists(proj_path), f"Path {proj_path} does not exist"
 print(f"{proj_path=}")
+assert os.path.exists(fits_root), f"Path {fits_root} does not exist"
 print(f"{fits_root=}")
+assert os.path.exists(data_root), f"Path {data_root} does not exist"
 print(f"{data_root=}")
