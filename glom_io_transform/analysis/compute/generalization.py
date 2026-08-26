1e7c5c0a0e6320ac3c4a15f64259da85230dc139
Sina Tootoonian <sina.tootoonian@gmail.com>
2026-08-26 08:54:13 +0100

    Fix condition to check for alpha in default_cache_file function

--- glom_io_transform/analysis/compute/generalization.py
+++ glom_io_transform/analysis/compute/generalization.py
@@ -127,7 +127,7 @@ def default_cache_file(base, splits=None):
         suffix += f"_loss={base.loss}"
     if getattr(base, "matched", False):
         suffix += "_matched"
-    if getattr(base, "alpha", False):
+    if getattr(base, "alpha", None) not None:
         suffix += f"_alpha={base.alpha}"
     all_specs = sorted({str(sp[2]) for sp in (splits or ()) if len(sp) > 2})
     specs = [sp for sp in all_specs if sp != "max"]
